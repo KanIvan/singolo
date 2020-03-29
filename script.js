98e1aae55d04b1da24a6b2b7bc1f08e6b6a0a80a
KanIvan <60774013+KanIvan@users.noreply.github.com>
2020-03-29 20:44:24 +0300

    docs: add Responsive

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ MENU_PORTF.addEventListener('click', (event) => {
 });
 
 GALERY.addEventListener('click', (event) => {
-    GALERY.querySelectorAll('a').forEach(el => el.classList.remove('active'));
+    GALERY.querySelectorAll('div').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
 });
 
